142093b39704e9bc5e779ea7cac1a65c8b3d4fc7
jdcampolargo <jdcampolargo@hotmail.com>
2022-11-20 14:52:38 -0500

    Initial Changes

--- _data/main.js
+++ _data/main.js
@@ -1,8 +1,8 @@
 module.exports = {
     en: {
-        welcome: "Welcome",
-        siteDescription: `<p>This is starter for <a class="c-text-link" href="https://www.11ty.dev/">eleventy ssg</a>.</p>
-                            <p>It's built with nunjuks and sass also it has rollup config for your js</p>
+        welcome: "Bienvenido a Carajo!",
+        siteDescription: `<p> ¿Viniste a USA recientemente? ¿Quieres estudiar? ¿Te gustaría progresar?.</p>
+                            <p>Agarra un "carajito" y empieza a aprender!</p>
 <p>For site serving you can use <a class="c-text-link" href="https://caddyserver.com/">caddy</a>, <a class="c-text-link" href="https://nginx.org">nginx</a>
  or use <a class="c-text-link" href="https://vercel.com/">vercel</a> or <a class="c-text-link" href="https://www.netlify.com/">netlify</a></p>`,
         latest: 'latest',
