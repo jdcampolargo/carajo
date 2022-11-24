fa0eeea4775fb6d9082931a14f94fa05d359a9d8
jdcampolargo <jdcampolargo@hotmail.com>
2022-11-24 09:24:00 -0600

    Updates

--- _data/main.js
+++ _data/main.js
@@ -2,9 +2,7 @@ module.exports = {
     en: {
         welcome: "Bienvenido a Carajo!",
         siteDescription: `<p> ¿Viniste a USA recientemente? ¿Quieres estudiar? ¿Te gustaría progresar?.</p>
-                            <p>Agarra un "carajito" y empieza a aprender!</p>
-<p>For site serving you can use <a class="c-text-link" href="https://caddyserver.com/">caddy</a>, <a class="c-text-link" href="https://nginx.org">nginx</a>
- or use <a class="c-text-link" href="https://vercel.com/">vercel</a> or <a class="c-text-link" href="https://www.netlify.com/">netlify</a></p>`,
+                            <p>Agarra un "carajito" y empieza a aprender!</p>`,
         latest: 'latest',
         post: 'post',
         posts: 'posts',
